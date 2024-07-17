a8baafa76b78a2dc7079f56f1ef3927ba9314e6f
MINT <mint.2978@gmail.com>
2024-07-17 14:51:15 +0700

    feat: ex-1/ex-2

--- ex-1.js
+++ ex-1.js
@@ -1,5 +1,33 @@
 function findStudentIndex(students, searchStudent) {
-	// เริ่มเขียนโค้ดตรงนี้จ้า
+  // เริ่มเขียนโค้ดตรงนี้จ้า
+  let left = 0;
+  let right = students.length - 1;
+  let index = -1;
+  while (left <= right) {
+    let middle = Math.floor((left + right) / 2);
+    if (students[middle] === searchStudent) {
+      index = middle;
+      break;
+    } else if (students[middle] < searchStudent) {
+      left = middle + 1;
+    } else if (students[middle] > searchStudent) {
+      right = middle - 1;
+    }
+  }
+  return index;
 }
 
+const students = [
+  "Andrew",
+  "Bobby",
+  "Diana",
+  "George",
+  "Hannah",
+  "Isaac",
+  "Jasmine",
+  "John",
+];
+console.log(findStudentIndex(students, "Isaac"));
+
 // ตอบคำถามตรงนี้จ้า
+//O(log n)
--- ex-2.js
+++ ex-2.js
@@ -1,3 +1,31 @@
 function findBookIndex(books, searchTitle) {
   // เริ่มเขียนโค้ดตรงนี้จ้า
+  let left = 0;
+  let right = books.length - 1;
+  let index = -1;
+  while (left <= right) {
+    let middle = Math.floor((left + right) / 2);
+    if (books[middle].title === searchTitle) {
+      index = middle;
+      break;
+    } else if (books[middle].title < searchTitle) {
+      left = middle + 1;
+    } else if (books[middle].title > searchTitle) {
+      right = middle - 1;
+    }
+  }
+  return index;
 }
+
+const books = [
+  { title: "A Tale of Two Cities", author: "Charles Dickens" },
+  { title: "Brave New World", author: "Aldous Huxley" },
+  { title: "Catch-22", author: "Joseph Heller" },
+  { title: "Don Quixote", author: "Miguel de Cervantes" },
+  { title: "Ender's Game", author: "Orson Scott Card" },
+  { title: "Fahrenheit 451", author: "Ray Bradbury" },
+  { title: "Gone with the Wind", author: "Margaret Mitchell" },
+  { title: "Harry Potter", author: "J.K. Rowling" },
+];
+
+console.log(findBookIndex(books, "Harry Potter"));
